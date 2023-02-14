48ef32d98fde9d929c509bb4cfecf3ff26390803
eunbi <103248831+bi1004@users.noreply.github.com>
2023-02-14 16:19:51 +0900

    🎈notice

--- src/model/NoticeDAO.java
+++ src/model/NoticeDAO.java
@@ -18,46 +18,47 @@ import util.DBUtil2;
 
 @Repository
 public class NoticeDAO {
-
+	
 	// 글 추가
-	public void insertNotice(Notice ndto) {
-		EntityManager em = DBUtil.getEntityManager();
-		EntityTransaction tx = em.getTransaction();
-
-		try {
-			tx.begin();
-			em.persist(new Notice(ndto.getNoticeTitle(), ndto.getNoticeContent(), 0));
-			tx.commit();
-		} catch (Exception e) {
-			tx.rollback();
-			e.printStackTrace();
-		} finally {
-			em.close();
-		}
-	}
-
-//	public static NoticeDTO insertNotice(NoticeDTO ndto) throws SQLException {
+//	public void insertNotice(Notice ndto)  {
 //		EntityManager em = DBUtil.getEntityManager();
 //		EntityTransaction tx = em.getTransaction();
+//		
 //		try {
 //			tx.begin();
-//			em.persist(ndto);
+//			Notice notice = new Notice(ndto.getNoticeTitle(), ndto.getNoticeContent(), 0);
+//			em.persist(notice);
 //			tx.commit();
-//			
 //		} catch (Exception e) {
 //			tx.rollback();
 //			e.printStackTrace();
-//			
-//		} finally {
+//		}finally {
 //			em.close();
 //		}
-//		
-//        return ndto;
-//    }
-//	
+//	}
+	
+	public static Notice insertNotice(Notice ndto) throws SQLException {
+		EntityManager em = DBUtil.getEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		try {
+			tx.begin();
+			em.persist(ndto);
+			tx.commit();
+			
+		} catch (Exception e) {
+			tx.rollback();
+			e.printStackTrace();
+			
+		} finally {
+			em.close();
+		}
+		
+        return ndto;
+    }
+	
 
 	// 글 수정
-	// 내용, 제목만 변경
+	// 내용, 제목만 변경 
 	/*
 	 * public boolean updateNotice2(NoticeDTO dto) throws SQLException { Connection
 	 * con = null; PreparedStatement pstmt = null;
@@ -73,93 +74,88 @@ public class NoticeDAO {
 	 * 
 	 * }
 	 */
-
-	public void updateNotice(Notice no) {
+	
+	public void updateNotice(Notice no){
 		EntityManager em = DBUtil.getEntityManager();
 		EntityTransaction tx = em.getTransaction();
 		Notice notice = null;
 
+
 		try {
 			tx.begin();
 			notice = em.find(Notice.class, no.getNoticeNo());
-
-			// 공지사항이 존재하면 null 아님, 내용 변경
-			if (notice != null) {
+			//공지사항이 존재하면 null 아님, 내용 변경
+			if(notice !=null) {
 				System.out.println("update 전 : " + notice);
-				notice.setNoticeContent(no.getNoticeContent()); // 게시물
-				notice.setNoticeTitle(no.getNoticeTitle()); // 제목
-			} else {
+				notice.setNoticeContent(no.getNoticeContent()); //게시물
+				notice.setNoticeTitle(no.getNoticeTitle()); //제목
+			}else {
 				System.out.println("업데이트 하려는 게시물을 찾지 못했습니다");
 			}
-
 			em.persist(notice);
 			tx.commit();
-		} catch (Exception e) {
+		}catch(Exception e) {
 			tx.rollback();
 			e.printStackTrace();
-		} finally {
+		}finally {
 			em.close();
 		}
 	}
-
-	// 글 삭제
-	// Long noticeNo
-	public void deleteNotice(Long noticeNo) {
-		EntityManager em = DBUtil.getEntityManager();
-		EntityTransaction tx = em.getTransaction();
-		
-		try {
-			tx.begin();
-			Notice notice = em.find(Notice.class, noticeNo);
 			
-			if (notice != null) {
-				em.remove(notice);
-			} else {
-				System.out.println("이미 삭제 된 게시글입니다.");
-			}
-			tx.commit();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			em.close();
-		}
-	}
 
-	// 상세조회
-	public NoticeDTO view(Long noticeNo, boolean flag) throws SQLException {
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rset = null;
-		NoticeDTO dto = null;
-		try {
-			con = DBUtil2.getConnection();
-
-			if (flag) {
-				pstmt = con.prepareStatement("update notice set ViewCount = ViewCount + 1 where noticeNo = ?");
-				pstmt.setLong(1, noticeNo);
-				if (pstmt.executeUpdate() == 0) {
-					pstmt.close();
-					pstmt = null;
+		// 글 삭제
+		//Long noticeNo
+		public void deleteNotice(Long noticeNo) {
+			EntityManager em = DBUtil.getEntityManager();
+			EntityTransaction tx = em.getTransaction();
+			try {
+				tx.begin();
+				Notice notice = em.find(Notice.class, noticeNo);
+				if (notice != null) {
+					em.remove(notice);
+				}else {
+					System.out.println("이미 삭제 된 게시글입니다.");
 				}
+				tx.commit();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				em.close();
 			}
-			pstmt = con.prepareStatement("select * from notice where noticeNo = ?");
-			pstmt.setLong(1, noticeNo);
-			rset = pstmt.executeQuery();
-			
-			if (rset.next()) {
-				dto = new NoticeDTO(noticeNo, rset.getString("noticeContent"), rset.getDate("noticeRegdate"),
-						rset.getString("noticeTitle"), rset.getInt("viewCount"));
-			}
-		} catch (SQLException s) {
-			s.printStackTrace();
-			throw s;
-		} finally {
-			DBUtil2.close(con, pstmt, rset);
 		}
-		return dto;
-
-	}
+	
+	// 상세조회
+		public NoticeDTO view(Long noticeNo, boolean flag) throws SQLException{
+			Connection con = null;	
+			PreparedStatement pstmt = null;
+			ResultSet rset = null;
+			NoticeDTO dto = null;
+			try {
+				con = DBUtil2.getConnection();
+				
+				if(flag) {
+					pstmt = con.prepareStatement("update notice set ViewCount = ViewCount + 1 where noticeNo = ?");
+					pstmt.setLong(1, noticeNo);
+					if(pstmt.executeUpdate() == 0) {
+						pstmt.close();
+						pstmt = null;
+					}
+				}
+				pstmt = con.prepareStatement("select * from notice where noticeNo = ?");
+				pstmt.setLong(1, noticeNo);
+				rset = pstmt.executeQuery();
+				if(rset.next()) {
+					dto = new NoticeDTO(noticeNo, rset.getString("noticeContent"), rset.getDate("noticeRegdate"), rset.getString("noticeTitle"), rset.getInt("viewCount"));
+				}
+			}catch(SQLException s) {
+				s.printStackTrace();
+				throw s;
+			}finally {
+				DBUtil2.close(con, pstmt, rset);
+			}
+			return dto;
+			
+		}
 
 	// 전체 글 조회
 //		public ArrayList<NoticeDTO> list() throws SQLException{
@@ -187,23 +183,29 @@ public class NoticeDAO {
 //			return list;
 //		}
 
-	public List<Notice> list() throws Exception {
-		EntityManager em = DBUtil.getEntityManager();
 
-		List<Notice> list = null;
-		try {
-			String sql = "select n from Notice n";
-			list = em.createQuery(sql).getResultList();
 
-			list.forEach(v -> System.out.println(v));
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw e;
-		} finally {
-			em.close();
+		public List<Notice> list() throws Exception{
+			EntityManager em = DBUtil.getEntityManager();
+			
+			List<Notice> list = null;
+			try {
+				String sql = "select n from Notice n";
+				list = em.createQuery(sql).getResultList();
+				
+				list.forEach(v->System.out.println(v));
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+				throw e;
+			}finally {
+				em.close();
+			}
+			return list;
 		}
-		return list;
-	}
 
+	
+	
+		
+	
 }
