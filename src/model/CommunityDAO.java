cfc0ed2117f9c98cb31df3c942817a23686c0cb6
PHyeinP <122508872+PHyeinP@users.noreply.github.com>
2023-02-15 11:04:44 +0900

    🐱 DAO 내용 수정

--- src/model/CommunityDAO.java
+++ src/model/CommunityDAO.java
@@ -1,243 +1,201 @@
-package controller;
+package model;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.junit.Test;
+import org.springframework.stereotype.Repository;
 
-import com.google.gson.JsonObject;
-
-import model.CommunityDAO;
 import model.domain.CommunityDTO;
+import model.domain.entity.Community;
+import model.domain.entity.StudyMembers;
+import util.DBUtil;
+import util.DBUtil2;
 
-@Controller
-@RequestMapping("Community")
-public class CommunityController {
-
-	@Autowired
-	public CommunityDAO comdao;
-	
-	
-	//글쓰기화면
-	@RequestMapping(value = "/writeform", method = RequestMethod.GET, produces="application/json;charset=UTF-8")
-	public String writeform() {
-		return "forward:/comm/write.jsp";
-	}
+@Repository
+public class CommunityDAO {
 	
 	//입력
-	@RequestMapping(value = "/write", method = RequestMethod.POST, produces="application/json;charset=UTF-8")
-	public String write(Model model, @ModelAttribute CommunityDTO dto) throws Exception{
-		System.out.println("insert()----------");
-		System.out.println(dto);
-		if(dto.getComContent() == null || dto.getComContent().trim().length() == 0 || dto.getComPw() == null || dto.getComPw().trim().length() == 0 || 
-				dto.getComTitle() == null || dto.getComTitle().trim().length() == 0 || dto.getSubject() == null) {
-			throw new RuntimeException("입력값이 충분하지 않습니다.");
-		}
-		CommunityDTO dto2 = comdao.write(dto);
-		model.addAttribute("dto", dto2);
-
-//		return "forward:/comm/read.jsp";
-		return "redirect:/Community/view/"+dto2.getComNo(); //상세 글 페이지로 이동
-//		return "redirect:list";
-	}
-
-
-
-	//읽기
-	@RequestMapping(value = "/view/{comNo}", method = RequestMethod.GET, produces="application/json;charset=UTF-8")
-	public String view(Model model, @PathVariable long comNo) throws SQLException {
-		System.out.println("view()------------"+comNo);
-
-		CommunityDTO dto = comdao.view(comNo, true);
-
-		if(dto == null) {
-			throw new RuntimeException("게시물이 존재하지 않습니다.");
-		}else {
-			model.addAttribute("dto", dto);
-		}
-		return "forward:/comm/read.jsp";
-	}
-
-
-
-	//목록 보기
-	@RequestMapping(value = "/list", method = RequestMethod.GET, produces="application/json;charset=UTF-8")
-	public String list(Model model) throws Exception{
-		System.out.println("list()------------");
-
-		model.addAttribute("list", comdao.list());
-		return "forward:/comm/allView.jsp";
-	}
-
-
-
-	//수정화면(read.jsp에서 수정버튼 클릭시 실행되는 로직)
-	@RequestMapping(value = "/updateform", method = RequestMethod.POST, produces="application/json;charset=UTF-8")
-	public String updateForm(Model model, @RequestParam("comNo") long comNo, @RequestParam("comPw") String comPw) throws SQLException{
-		System.out.println("updateForm()---------"+comNo);
-
-		if(comNo == 0 || comPw == null || comPw.trim().length() == 0) { //입력 덜했을때
-			throw new RuntimeException("입력값이 충분하지 않습니다.");
-		}else {//일단입력은했을때
-			CommunityDTO dto = comdao.view(comNo, false);
+	public CommunityDTO write(CommunityDTO dto) throws Exception{
+		EntityManager em = DBUtil.getEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		try {
+			tx.begin();
 			
-			if(dto.getComPw().equals(comPw)) {
-				model.addAttribute("dto", dto);
-			}else {
-				throw new RuntimeException("비밀번호가 틀렸습니다.");
-			}
-		}
-		return "forward:/comm/update.jsp";
-	}
-
-
-	//수정
-	@RequestMapping(value = "/update", method = RequestMethod.POST, produces="application/json;charset=UTF-8")
-	public String update(Model model, @ModelAttribute CommunityDTO dto) throws SQLException{
-		System.out.println("update()---------");
-		if(dto.getComContent() == null || dto.getComContent().trim().length() == 0 || dto.getComPw() == null || dto.getComPw().trim().length() == 0 || 
-				dto.getComTitle() == null || dto.getComTitle().trim().length() == 0 || dto.getSubject() == null) {
-			throw new RuntimeException("입력값이 충분하지 않습니다.");
-		}
-		boolean result = comdao.update(dto);
-		if(result) {
-			model.addAttribute("comNo", dto.getComNo());
-		}else {
-			throw new RuntimeException("게시물이 존재하지 않거나 비밀번호가 틀렸습니다.");
-		}
-
-		return "redirect:view/"+dto.getComNo();
-//		return "redirect:list";
-	}
-	
-	
-	
-	//삭제
-	@RequestMapping(value = "/delete", method = RequestMethod.POST, produces="application/json;charset=UTF-8")
-	public String delete(@RequestParam("comNo") long comNo, @RequestParam("comPw") String comPw) throws Exception {
-		System.out.println("delete()---------");
-		if(comNo == 0 || comPw == null || comPw.trim().length() == 0) {
-			throw new RuntimeException("입력값이 충분하지 않습니다.");
-		}
-		boolean result = comdao.delete(comNo, comPw);
-		if(!result) {
-			throw new RuntimeException("게시물이 존재하지 않거나 비밀번호가 틀렸습니다.");
-		}
-		return "redirect:list";
-	}
-
-	
-	//검색
-	@RequestMapping(value = "/search", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
-	public String search(Model model, @RequestParam String searchType, @RequestParam String searchText) throws Exception{
-		System.out.println("search()---------------"+searchType+" "+searchText);
-		
-		if(searchText == null || searchText.trim().length() == 0) {
-			throw new RuntimeException("검색어를 입력하세요.");
+			StudyMembers member = em.find(StudyMembers.class, dto.getMemberid());
+			Community community = new Community(dto.getComTitle(), dto.getSubject(), dto.getComContent(), dto.getComPw(), 0);
+			community.setStudymembers(member);
+			em.persist(community);//entity 영속성 저장
+			
+			CommunityDTO dto2 = CommunityDTO.fromEntity(community);//entity를 dto로 변환
+			
+			tx.commit();//db까지저장
+			
+			return dto2;//entity를 dto로 변환한 것 전송
+			
+		}catch(Exception e) {
+			tx.rollback();
+			throw e;
+		}finally {
+			em.close();
 		}
 		
-		List<CommunityDTO> list = comdao.search(searchType, searchText);
-		model.addAttribute("list", list);
-		return "forward:/comm/searchView.jsp";
-	}
-
-
-	
-	
-	//예외 처리에 대한 중복 코드를 분리해서 예외처리 전담 메소드
-	@ExceptionHandler(Exception.class)
-	public String totalEx(Exception e, HttpServletRequest req) { 
-		System.out.println("예외 처리 전담");
-		e.printStackTrace(); //개발자 관점에서 필요한 정보, 서버 콘솔창에만 출력
-
-		req.setAttribute("errorMsg", e.getMessage());
-		return "forward:/comm/error.jsp"; 
 	}
 	
-	//이미지파일 업로드
-	@RequestMapping(value = "fileUpload.do", method = RequestMethod.POST, produces="application/json;charset=UTF-8")
-	@ResponseBody
-	public String fileUpload(HttpServletRequest req, HttpServletResponse res, MultipartHttpServletRequest multiFile) throws Exception{
-		//Json 객체 생성
-		JsonObject json = new JsonObject();
-		//Json 객체 출력하기 위해 PrintWriter 생성
-		PrintWriter printWriter = null;
-		OutputStream out = null;
-		//파일을 가져오기 위해 MultipartHttpServletRequest의 getFile 메서드 사용
-		MultipartFile file = multiFile.getFile("upload");
-		//파일이 비어있지 않고(비어있으면 null반환)
-		if(file != null) {
-			//파일 사이즈가 0보다 크고, 파일이름이 공백이 아닐때
-			if(file.getSize()>0 && StringUtils.isNotBlank(file.getName())) {
-				if(file.getContentType().toLowerCase().startsWith("image/")) {
-					try {
-						//파일 이름 
-						String fileName = file.getName();
-						//파일 내용
-						byte[] bytes = file.getBytes();
-						//파일이 실제 저장되는 경로(/team2_studyroom/img)
-						String uploadPath = req.getServletContext().getRealPath("/img");
-						System.out.println("====="+uploadPath);
-						//저장되는 파일에 경로 설정
-						File uploadFile = new File(uploadPath);
-						if(!uploadFile.exists()) {
-							uploadFile.mkdirs();
-						}
-						//UUID : 사용고유성이 보장되는 id. 파일명 중복방지 랜덤생성
-						fileName = UUID.randomUUID().toString();
-						//업로드 경로+파일이름을 줘서 데이터를 서버에 전송
-						uploadPath = uploadPath+"/"+fileName;
-						out = new FileOutputStream(new File(uploadPath));
-						out.write(bytes);
-						
-						//클라이언트에 추가
-						printWriter = res.getWriter();
-						res.setContentType("text/html");
-						
-						//파일 연결되는 url 주소 설정
-						String fileUrl = req.getContextPath()+"/img/"+fileName;
-						System.out.println("====="+fileUrl);
-						//생성된 json 객체를 이용해 파일 업로드+이름+주소를 ckEditor에 전송
-						json.addProperty("uploaded", 1);
-						json.addProperty("fileName", fileName);
-						json.addProperty("url", fileUrl);
-						
-						printWriter.println(json);
-					}catch(IOException e) {
-						e.printStackTrace();
-					}finally {
-						if(out != null) {
-							out.close();
-						}
-						if(printWriter != null) {
-							printWriter.close();
-						}
-					}
+	//읽기
+	public CommunityDTO view(long comNo, boolean flag) throws SQLException{
+//		EntityManager em = DBUtil.getEntityManager();
+//		EntityTransaction tx = em.getTransaction();
+		
+		Connection con = null;	
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		CommunityDTO dto = null;
+		try {
+			con = DBUtil2.getConnection();
+			
+			if(flag) {
+				pstmt = con.prepareStatement("update community set comViewCount = comViewCount + 1 where comNo = ?");
+				pstmt.setLong(1, comNo);
+				if(pstmt.executeUpdate() == 0) {
+					pstmt.close();
+					pstmt = null;
+					return dto; //여기서 메소드 종료
 				}
 			}
+			pstmt = con.prepareStatement("select * from community where comNo = ?");
+			pstmt.setLong(1, comNo);
+			rset = pstmt.executeQuery();
+			if(rset.next()) {
+				dto = new CommunityDTO(comNo, rset.getString("comTitle"), rset.getString("memberid"), rset.getString("subject"), rset.getString("comPw"), 
+						rset.getDate("comRegdate"), rset.getString("comContent"), rset.getInt("comViewCount"));
+			}
+		}catch(SQLException s) {
+			s.printStackTrace();
+			throw s;
+		}finally {
+			DBUtil2.close(con, pstmt, rset);
 		}
-		return null;
+		return dto;
+		
+	}
+
+	//목록
+	public List<Community> list() throws Exception{
+		EntityManager em = DBUtil.getEntityManager();
+		
+		List<Community> list = null;
+		try {
+			String sql = "select c from Community c order by c.comNo desc";
+			list = em.createQuery(sql).getResultList();
+			
+			//list.forEach(v->System.out.println(v));
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			throw e;
+		}finally {
+			em.close();
+		}
+		return list;
 	}
 	
+	
+	//수정
+	public boolean update(CommunityDTO dto) throws SQLException{
+		Connection con = null;	
+		PreparedStatement pstmt = null;
+		try {
+			con = DBUtil2.getConnection();
+			pstmt = con.prepareStatement("update community set comTitle = ?, subject = ?, comContent = ? where comNo = ? and comPw = ?");
+			pstmt.setString(1, dto.getComTitle());
+			pstmt.setString(2, dto.getSubject());
+			pstmt.setString(3, dto.getComContent());
+			pstmt.setLong(4, dto.getComNo());
+			pstmt.setString(5, dto.getComPw());
+			
+			int result = pstmt.executeUpdate();
+			if(result != 0) {
+				return true;
+			}
+		}catch(SQLException s) {
+			s.printStackTrace();
+			throw s;
+		}finally {
+			DBUtil2.close(con, pstmt);
+		}
+		return false;
+		
+	}
+
+	
+	//삭제
+	public boolean delete(long comNo, String comPw) throws Exception{
+		Connection con = null;	
+		PreparedStatement pstmt = null;
+		try{
+			con = DBUtil2.getConnection();
+			pstmt = con.prepareStatement("delete from community where comNo = ? and comPw = ?");
+			pstmt.setLong(1, comNo);
+			pstmt.setString(2, comPw);
+			
+			int result = pstmt.executeUpdate();
+			if(result != 0) {
+				return true;
+			}
+		}catch(SQLException s) {
+			s.printStackTrace();
+			DBUtil2.close(con, pstmt);
+			throw new Exception("게시물이 존재하지 않거나 비밀번호가 틀렸습니다.");
+		}
+		return false;
+	}
+	
+	//검색
+	public List<CommunityDTO> search(String searchType, String searchText) {
+		EntityManager em = DBUtil.getEntityManager();
+		List<Community> all = null;
+		List<CommunityDTO> all2 = new ArrayList<CommunityDTO>();
+		
+		if("title".equals(searchType)) {
+			all = em.createNamedQuery("Community.findByComTitle").setParameter("comTitle", "%"+searchText+"%").getResultList();
+//			System.out.println(all);
+			
+		}else if("content".equals(searchType)) {
+			all = em.createNamedQuery("Community.findByComContent").setParameter("comContent", "%"+searchText+"%").getResultList();
+//			System.out.println(all);
+			
+		}else if("writer".equals(searchType)) {
+			all = em.createNamedQuery("Community.findByMemberid").setParameter("memberid", "%"+searchText+"%").getResultList();
+			System.out.println(all);
+		}
+		for(Community v:all) {
+			all2.add(CommunityDTO.fromEntity(v));
+			System.out.println(all2);
+		}
+		return all2;
+	}
+	
+	
+	@Test
+	public void dataTest() {
+		try {
+//			write(new CommunityDTO(0, "제목", "아이디", "말머리", "비번", null, "내용..", 0));
+//			list();
+			search("writer", "te");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
 }
